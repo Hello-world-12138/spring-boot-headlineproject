8c03a56d2dc52d1f9fa585e41d498e5eda76ee18
Hello-world-12138 <als20050228@qq.com>
2025-11-10 16:42:08 +0800

    微头条的初始构建

---
+++ src/test/java/com/amk/test/JwtTest.java
@@ -0,0 +1,32 @@
+package com.amk.test;
+
+import com.amk.utils.JwtHelper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 阿明楷
+ * @Date 2025/11/10:16:01
+ * @See:
+ */
+@SpringBootTest(classes = com.amk.Main.class)
+public class JwtTest {
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @Test
+    public void test(){
+        //生成 传入用户标识
+        String token = jwtHelper.createToken(1L);
+        System.out.println("token = " + token);
+
+        //解析用户标识
+        int userId = jwtHelper.getUserId(token).intValue();
+        System.out.println("userId = " + userId);
+
+        //校验是否到期! false 未到期 true到期
+        boolean expiration = jwtHelper.isExpiration(token);
+        System.out.println("expiration = " + expiration);
+    }
+}
